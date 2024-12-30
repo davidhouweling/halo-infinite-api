069b2695cdb3d826cb9e498856b3b92c1b66a3b0
David Houweling <houweling@outlook.com>
2024-12-30 21:28:38 +1100

    Remove request policy usage (drops cockatiel dependency)

--- src/core/request-policy.ts
+++
@@ -1,7 +0,0 @@
-import { handleType, retry } from "cockatiel";
-import { RequestError } from "../util/request-error";
-
-export const unauthorizedRetryPolicy = retry(
-  handleType(RequestError, (error) => error.response.status === 401),
-  { maxAttempts: 2 }
-);
